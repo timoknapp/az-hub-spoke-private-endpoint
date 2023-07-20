4844281ff8009d896f69026250e7cbcef65b4305
timoknapp <mail@timoknapp.com>
2023-07-20 16:17:26 +0200

    feat: add vm extension to install MSSQL tools

--- util/install_mssql.sh
+++ util/install_mssql.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-dpkg --configure -a
-apt-get -y update
+sudo dpkg --configure -a
+sudo apt-get -y update
 
 # install MSSQL Tools
-apt-get -y install mssql-tools18 unixodbc-dev
+sudo apt-get -y install mssql-tools18 unixodbc-dev
 
 # add MSSQL Tools to PATH
 echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
