efc46863c5a1bb8f527f28a32d5e16cccb15e1e7
timoknapp <mail@timoknapp.com>
2023-07-21 15:03:02 +0200

    feat: update install mssql script

--- util/install_mssql.sh
+++ util/install_mssql.sh
@@ -9,4 +9,4 @@ sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18 unixodbc-dev
 
 # add MSSQL Tools to PATH
 echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> /home/azureuser/.bashrc
-source /home/azureuser/.bashrc
+# source /home/azureuser/.bashrc
