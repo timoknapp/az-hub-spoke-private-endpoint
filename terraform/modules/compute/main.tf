987fb6ae003af123d84622b42a7a953bfc3718f9
timoknapp <mail@timoknapp.com>
2023-08-09 18:12:25 +0200

    feat(tf): another spoke vm

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -32,10 +32,60 @@ resource "azurerm_virtual_machine" "vm_spoke1" {
     }
 }
 
+# Create a Ubuntu based VM in Spoke 2
+resource "azurerm_virtual_machine" "vm_spoke2" {
+    name                  = "vm-client-spoke-${var.spoke2_location}"
+    resource_group_name   = var.resource_group_name
+    location              = var.spoke2_location
+    network_interface_ids = [azurerm_network_interface.nic_vm2.id]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_image_reference {
+        publisher = "canonical"
+        offer     = "0001-com-ubuntu-server-focal"
+        sku       = "20_04-lts-gen2"
+        version   = "latest"
+    }
+
+    storage_os_disk {
+        name              = "disk-vm-client-spoke-${var.spoke2_location}"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Standard_LRS"
+        os_type           = "Linux"
+    }
+
+    os_profile {
+        computer_name  = "vm-client-spoke-${var.spoke2_location}"
+        admin_username = var.vm_username
+        admin_password = var.vm_password
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+}
+
 # Create VM Extension CustomScript for VM 1
 resource "azurerm_virtual_machine_extension" "vm_extension_vm1" {
     name                 = "install_mssql"
     virtual_machine_id   = azurerm_virtual_machine.vm_spoke1.id
+    publisher            = "Microsoft.Azure.Extensions"
+    type                 = "CustomScript"
+    type_handler_version = "2.1"
+
+    protected_settings = <<SETTINGS
+    {
+        "fileUris": ["https://raw.githubusercontent.com/timoknapp/az-hub-spoke-private-endpoint/master/util/install_mssql.sh"],
+        "commandToExecute": "sh install_mssql.sh"
+    }
+    SETTINGS
+}
+
+# Create VM Extension CustomScript for VM 2
+resource "azurerm_virtual_machine_extension" "vm_extension_vm2" {
+    name                 = "install_mssql"
+    virtual_machine_id   = azurerm_virtual_machine.vm_spoke2.id
     publisher            = "Microsoft.Azure.Extensions"
     type                 = "CustomScript"
     type_handler_version = "2.1"
@@ -57,6 +107,19 @@ resource "azurerm_network_interface" "nic_vm1" {
     ip_configuration {
         name                          = "ipconfig1"
         subnet_id                     = "${var.snet_id_spoke1}"
+        private_ip_address_allocation = "Dynamic"
+    }
+}
+
+# Create NIC for VM in Spoke 2
+resource "azurerm_network_interface" "nic_vm2" {
+    name                = "nic-vm-spoke-${var.spoke2_location}-001"
+    location            = var.spoke2_location
+    resource_group_name = var.resource_group_name
+
+    ip_configuration {
+        name                          = "ipconfig1"
+        subnet_id                     = "${var.snet_id_spoke2}"
         private_ip_address_allocation = "Dynamic"
     }
 }
@@ -86,8 +149,39 @@ resource "azurerm_network_security_group" "nsg_vm1" {
     }
 }
 
+# Create NSG for NIC in Spoke 2
+resource "azurerm_network_security_group" "nsg_vm2" {
+    name                = "nsg-nic-spoke-vm-${var.spoke2_location}"
+    location            = var.spoke2_location
+    resource_group_name = var.resource_group_name
+
+    security_rule {
+        access = "Allow"
+        destination_address_prefix = "*"
+        destination_address_prefixes = []
+        destination_application_security_group_ids = []
+        destination_port_range = "22"
+        destination_port_ranges = []
+        direction = "Inbound"
+        name = "AllowTagSSHInbound"
+        priority = 100
+        protocol = "TCP"
+        source_address_prefix = "VirtualNetwork"
+        source_address_prefixes = []
+        source_application_security_group_ids = []
+        source_port_range = "*"
+        source_port_ranges = []
+    }
+}
+
 # Associate NSG to NIC 1
 resource "azurerm_network_interface_security_group_association" "nsg-nic-vm1" {
     network_interface_id      = azurerm_network_interface.nic_vm1.id
     network_security_group_id = azurerm_network_security_group.nsg_vm1.id
+}
+
+# Associate NSG to NIC 2
+resource "azurerm_network_interface_security_group_association" "nsg-nic-vm2" {
+    network_interface_id      = azurerm_network_interface.nic_vm2.id
+    network_security_group_id = azurerm_network_security_group.nsg_vm2.id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
+variable "resource_group_name" {
+    type = string
+    default     = "rg-hub-spoke"
+    description = "Resource group name."
+}
+
 variable "resource_group_location" {
     type = string
     default     = "westeurope"
@@ -21,27 +27,3 @@ variable "spoke2_location" {
     default     = "northeurope"
     description = "Location of Spoke 2."
 }
-
-variable "resource_group_name" {
-    type = string
-    default     = "rg-hub-spoke-tf"
-    description = "Resource group name."
-}
-
-# variable "vnet_hub_name" {
-#     type = string
-#     default     = "vnet-hub-${var.hub_location}-001"
-#     description = "Virtual network name hub."
-# }
-
-# variable "vnet_spoke1_name" {
-#     type = string
-#     default     = "vnet-spoke-${var.spoke1_location}-001"
-#     description = "Virtual network name spoke 1."
-# }
-
-# variable "vnet_spoke2_name" {
-#     type = string
-#     default     = "vnet-spoke-${var.spoke2_location}-001"
-#     description = "Virtual network name spoke 2."
-# }
